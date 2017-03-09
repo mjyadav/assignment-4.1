9be41715aeac3fe8ce7cbced02d593d508391390
mjyadav <mrityunjay.yadav0007@gmail.com>
2017-03-09 15:53:12 +0530

    assignment 4.1
    assignment 4.1

---
+++ src/Parent.java
@@ -0,0 +1,26 @@
+
+public class Parent {
+//What is
+//	displayed when
+//the following
+//	code is execute
+	private void method1() {
+		System.out.println("Parent's method1()");
+	}
+
+	public void method2() {
+		System.out.println("Parent's method2()");
+		method1();
+	}
+}
+
+class Child extends Parent {
+	public void method1() {
+		System.out.println("Child's method1()");
+	}
+
+	public static void main(String args[]) {
+		Parent p = new Child();
+		p.method2();
+	}
+}
